1eea6b937f1bfb8259b6d7659d37e36255da61e5
fagneba <francis.agneba@gmail.com>
2024-12-27 02:45:29 +0100

    Création et suppression de l'adresse du client

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Address;
 use App\Repository\AddressRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,4 +24,36 @@ class AccountController extends AbstractController
             'addresses' => $addresses,
         ]);
     }
+    // #[Route('/api/address', name: 'app_api_address', methods: ['POST'])]
+    // public function postAddress(
+    //     Request $req,
+    //     AddressRepository $addressRepository,
+    //     EntityManagerInterface  $manager
+    // ): Response {
+    //     $formData = $req->getPayload();
+    //     $user = $this->getUser();
+
+    //     $address = new Address();
+    //     $address->setName($formData->get('name'))
+    //         ->setClientName($formData->get('client_name'))
+    //         ->setStreet($formData->get('street'))
+    //         ->setCodePostal($formData->get('code_postal'))
+    //         ->setCity($formData->get('city'))
+    //         ->setState($formData->get('state'))
+    //         ->setUser($user)
+
+    //     ;
+    //     $manager->persist($address);
+    //     $manager->flush();
+
+    //     $addresses = $addressRepository->findByUser($user);
+
+    //     foreach ($addresses as $key => $address) {
+    //         $address->setUser(null);
+    //         $addresses[$key] = $address;
+    //     }
+
+
+    //     return $this->json($addresses);
+    // }
 }
