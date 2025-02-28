ec72cf4874d55ae9933f55f872719b10e3484222
fagneba <francis.agneba@gmail.com>
2025-02-28 19:17:35 +0100

    Administration des commandes

--- src/Controller/Admin/OrderCrudController.php
+++ src/Controller/Admin/OrderCrudController.php
@@ -3,10 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Order;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 
 class OrderCrudController extends AbstractCrudController
 {
@@ -15,13 +19,24 @@ class OrderCrudController extends AbstractCrudController
         return Order::class;
     }
 
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_EDIT, Action::INDEX)
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->add(Crud::PAGE_EDIT, Action::DETAIL);
+    }
 
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('client_name'),
+            TextField::new('delivery_address'),
+            TextField::new('quantity'),
+            MoneyField::new('order_cost')->setCurrency('EUR'),
+            MoneyField::new('taxe')->setCurrency('EUR'),
+            MoneyField::new('order_cost_ttc')->setCurrency('EUR'),
         ];
     }
 }
