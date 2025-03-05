abe026931f8350a5e34ab6f117edbdf74d4167fd
fagneba <francis.agneba@gmail.com>
2025-03-05 01:14:59 +0100

    Protection de la page Checkout

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -5,24 +5,35 @@ namespace App\Controller;
 use App\Services\CartServices;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CheckoutController extends AbstractController
 {
     private CartServices $cartServices;
+    private $session;
 
-    public function __construct(CartServices $cartServices)
+    public function __construct(CartServices $cartServices, RequestStack $requestStack)
     {
         $this->cartServices = $cartServices;
+        $this->session = $requestStack->getSession();
     }
 
     #[Route('/checkout', name: 'app_checkout')]
     public function index(): Response
     {
+
         $cart = $this->cartServices->getFullCart();
 
         if (!count($cart["items"])) {
             return $this->redirectToRoute('app_home');
+        }
+
+        $user = $this->getUser();
+
+        if (!$user) {
+            $this->session->set("next", "app_checkout");
+            return $this->redirectToRoute("app_login");
         }
 
         // $cart_json = json_encode($cart);
--- src/Security/AppCustomAuthenticator.php
+++ src/Security/AppCustomAuthenticator.php
@@ -2,19 +2,20 @@
 
 namespace App\Security;
 
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
-use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\SecurityRequestAttributes;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Http\SecurityRequestAttributes;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 
 class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
 {
@@ -22,7 +23,12 @@ class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
 
     public const LOGIN_ROUTE = 'app_login';
 
-    public function __construct(private UrlGeneratorInterface $urlGenerator) {}
+    private $session;
+
+    public function __construct(private UrlGeneratorInterface $urlGenerator, RequestStack $requestStack)
+    {
+        $this->session = $requestStack->getSession();
+    }
 
     public function authenticate(Request $request): Passport
     {
@@ -44,6 +50,12 @@ class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
     {
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
             return new RedirectResponse($targetPath);
+        }
+
+        $redirectPath = $this->session->get("next");
+
+        if ($redirectPath) {
+            return new RedirectResponse($this->urlGenerator->generate($redirectPath));
         }
 
         // For example:
