3203866f9f6625770b85bdb9e2931e4841feec7b
fagneba <francis.agneba@gmail.com>
2025-04-18 01:18:40 +0200

    Ajustement de la partie commande de notre page Account

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,32 +3,71 @@
 namespace App\Controller;
 
 use App\Entity\Address;
+use App\Services\CartServices;
+use App\Repository\OrderRepository;
 use App\Repository\AddressRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AccountController extends AbstractController
 {
+    private CartServices $cartServices;
+    private OrderRepository $orderRepo;
+    private $session;
+
+    public function __construct(
+        CartServices $cartServices,
+        RequestStack $requestStack,
+        OrderRepository $orderRepo
+    ) {
+        $this->cartServices = $cartServices;
+        $this->orderRepo = $orderRepo;
+        $this->session = $requestStack->getSession();
+    }
+
     #[Route('/account', name: 'app_account')]
     public function index(AddressRepository $addressRepository): Response
     {
+        $cart = $this->cartServices->getFullCart();
+
+        if (empty($cart["items"])) {
+            return $this->redirectToRoute('app_home');
+        }
+
         $user = $this->getUser();
+
+        if (!$user) {
+            $this->session->set("next", "app_account");
+            return $this->redirectToRoute("app_login");
+        }
+
+        $cart_json = json_encode($cart);
+
+        // Tu peux commenter ou ajuster si `createOrder` est temporaire
+        // $orderId = $this->createOrder($cart);
+        $orderId = null;
 
         $addresses = $addressRepository->findByUser($user);
 
         return $this->render('account/index.html.twig', [
             'controller_name' => 'AccountController',
             'addresses' => $addresses,
+            'cart' => $cart,
+            'orderId' => $orderId,
+            'cart_json' => $cart_json,
         ]);
     }
+
+    // Si tu veux activer cette route plus tard
     // #[Route('/api/address', name: 'app_api_address', methods: ['POST'])]
     // public function postAddress(
     //     Request $req,
     //     AddressRepository $addressRepository,
-    //     EntityManagerInterface  $manager
+    //     EntityManagerInterface $manager
     // ): Response {
     //     $formData = $req->getPayload();
     //     $user = $this->getUser();
@@ -40,9 +79,8 @@ class AccountController extends AbstractController
     //         ->setCodePostal($formData->get('code_postal'))
     //         ->setCity($formData->get('city'))
     //         ->setState($formData->get('state'))
-    //         ->setUser($user)
+    //         ->setUser($user);
 
-    //     ;
     //     $manager->persist($address);
     //     $manager->flush();
 
@@ -52,7 +90,6 @@ class AccountController extends AbstractController
     //         $address->setUser(null);
     //         $addresses[$key] = $address;
     //     }
-
 
     //     return $this->json($addresses);
     // }
