ecda1dfa9e5eb6d22ae719e951bf7e49e55bdd9d
fagneba <francis.agneba@gmail.com>
2025-05-12 19:17:47 +0200

    Ajout de la fonction __toString dans l'entité Order

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -315,4 +315,9 @@ class Order
 
         return $this;
     }
+
+    public function __toString(): string
+    {
+        return 'Order #' . $this->getId(); // ou n'importe quelle info représentative
+    }
 }
